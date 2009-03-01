34372a455ef56c3547c156a6e96957d07b5470a4
Denis Bilenko <denis@ag-projects.com>
2009-03-01 11:29:26 +0000

    updte version to 0.9.2 in __init__.py

--- msrplib/__init__.py
+++ msrplib/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2008-2009 AG Projects. See LICENSE for details
 
-version_info = (0, 9, 1)
+version_info = (0, 9, 2)
 __version__ = '%s.%s.%s' % version_info[:3]
 
 class MSRPError(Exception):
