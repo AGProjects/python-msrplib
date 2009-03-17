0d24044fc791dd259b1defe9926e56cfb38c868a
Denis Bilenko <denis@ag-projects.com>
2009-03-17 10:11:07 +0000

    set the timeout to 30 seconds

--- msrplib/connect.py
+++ msrplib/connect.py
@@ -103,7 +103,7 @@ class TimeoutMixin(object):
         return timeout(cls.seconds, *throw_args)
 
 class MSRPTimeout(MSRPError, TimeoutMixin):
-    seconds = 10
+    seconds = 30
 
 class MSRPConnectTimeout(MSRPTimeout):
     pass
