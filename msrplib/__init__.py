cd7028b15ce5e8d1da102e6d0cc4c8175eb79a8b
Adrian Georgescu <ag@ag-projects.com>
2009-03-17 19:33:51 +0000

    Increase version for the next release

--- msrplib/__init__.py
+++ msrplib/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2008-2009 AG Projects. See LICENSE for details
 
-version_info = (0, 9, 3)
+version_info = (1, 0, 0)
 __version__ = '%s.%s.%s' % version_info[:3]
 
 class MSRPError(Exception):
