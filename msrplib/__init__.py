5fe0a7cfa9356c0c61b27e0903c57896085f8b20
Adrian Georgescu <ag@ag-projects.com>
2009-10-29 21:52:55 +0000

    Increased version to 0.11 and added changelog entry

--- msrplib/__init__.py
+++ msrplib/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2008-2009 AG Projects. See LICENSE for details
 
-version_info = (0, 10, 0)
+version_info = (0, 11, 0)
 __version__ = '%s.%s.%s' % version_info[:3]
 
 class MSRPError(Exception):
