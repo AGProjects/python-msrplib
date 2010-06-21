10af6094d40b43d71dba93a4344f4bdeceb8cf56
Saul Ibarra <saul@ag-projects.com>
2010-06-21 10:27:17 +0000

    Raised version and updated changelog

--- msrplib/__init__.py
+++ msrplib/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2008-2009 AG Projects. See LICENSE for details
 
-version_info = (0, 11, 1)
+version_info = (0, 12, 0)
 __version__ = '%s.%s.%s' % version_info[:3]
 
 class MSRPError(Exception):
